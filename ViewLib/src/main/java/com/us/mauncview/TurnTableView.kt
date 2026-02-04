aa8b07cfc0bf6d7fd9f8b75d472913ee11aeaaf7
Time Will Remember Us <h16631031410@163.com>
2026-02-04 11:32:46 +0800

    1.优化转盘代码

--- ViewLib/src/main/java/com/us/mauncview/TurnTableView.kt
+++ ViewLib/src/main/java/com/us/mauncview/TurnTableView.kt
@@ -12,12 +12,12 @@ import android.view.View
 import android.view.animation.DecelerateInterpolator
 import androidx.annotation.ColorRes
 import androidx.annotation.RequiresApi
-import androidx.core.animation.addListener
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
 import androidx.core.content.ContextCompat
 import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.sin
-
 
 class TurnTableView @JvmOverloads constructor(
     context: Context,
@@ -27,6 +27,12 @@ class TurnTableView @JvmOverloads constructor(
 
     companion object {
         const val TAG = "TurnTableView"
+
+        // 转盘动画减速因子
+        const val DECELERATE_FACTOR = 2.0f
+
+        // 转盘动画持续时间
+        const val ANIM_DURATION = 5000L
     }
 
     // view的总宽度
@@ -36,73 +42,95 @@ class TurnTableView @JvmOverloads constructor(
     private var viewHeight = 0f
 
     // 圆心坐标X
-    private var centerCircleX = 0f
+    private var circleX = 0f
 
     // 圆心坐标Y
-    private var centerCircleY = 0f
+    private var circleY = 0f
 
     // 圆的半径
-    private var circleRadius: Float = 0f
+    private var circleRadius = 0f
 
     // 每个扇形的绘制的起始角度
-    private var sectorStartAngle = 0f
+    private var startAngle = 0f
 
     // 计算出每个扇形的间隔
-    private var sectorSweepAngle = 0f
+    private var sweepAngle = 0f
 
     // 画圆形背景的笔
-    private var turnTableBackGroundPaint: Paint = Paint().apply {
-        color = Color.GRAY
-        isAntiAlias = true
-        style = Paint.Style.FILL
-        strokeWidth = 8f
+    private val turnTableBackGroundPaint: Paint by lazy {
+        Paint().apply {
+            color = Color.GRAY
+            isAntiAlias = true
+            style = Paint.Style.FILL
+            strokeWidth = 8f
+        }
     }
 
     // 画圆形边框的笔
-    private var turnTableStrokePaint: Paint = Paint().apply {
-        color = obtainColorRes(R.color.black)
+    private val turnTableStrokePaint: Paint by lazy {
+        Paint().apply {
+            color = obtainColorRes(R.color.black)
+            isAntiAlias = true
+            style = Paint.Style.STROKE
+            strokeCap = Paint.Cap.ROUND
+            strokeWidth = 30f
+        }
+    }
+
+    // 画扇形的笔
+    private val turnAnglePaint: Paint by lazy {
+        Paint().apply {
+            isAntiAlias = true
+            style = Paint.Style.FILL
+        }
+    }
+
+    // 画字的笔
+    private var textPaint: Paint = Paint().apply {
+        color = Color.BLACK
         isAntiAlias = true
-        style = Paint.Style.STROKE
-        strokeCap = Paint.Cap.ROUND
-        strokeWidth = 30f
+        textSize = 40f
+        textAlign = Paint.Align.CENTER
     }
 
     private var colorResList = mutableListOf(
-        R.color.red,
-        R.color.blue,
-        R.color.orange,
-        R.color.yellow,
-        R.color.purple,
-        R.color.green
+        Color.parseColor("#FF0000"),
+        Color.parseColor("#008EFF"),
+        Color.parseColor("#FF3D00"),
+        Color.parseColor("#FFE500"),
+        Color.parseColor("#5C00FF"),
+        Color.parseColor("#00FF0A")
     )
 
     private var turnTableContentList = mutableListOf(
         "昆仑镜", "女娲石", "神农鼎", "崆峒印", "伏羲琴", "万灵血珠"
     )
 
-    // 画扇形的笔
-    private var turnTableSectorPaints: MutableList<Paint> = mutableListOf()
+    // 动画
+    private val valueAnimator: ValueAnimator by lazy {
+        ValueAnimator().apply {
+            setDuration(ANIM_DURATION)
+            interpolator = DecelerateInterpolator(DECELERATE_FACTOR)
+            addUpdateListener {
+                val value: Float = it.animatedValue as Float
+                // 控制sectorStartAngle在0到360之间
+                startAngle = (value % 360 + 360) % 360
+//                Log.e(TAG, "animatedValue:${startAngle}")
+                invalidate()
+            }
+            doOnStart {
+                Log.e(TAG, "doOnStart")
+            }
+            doOnEnd {
+                Log.e(TAG, "doOnEnd")
+            }
+        }
+    }
 
     init {
         circleRadius = 450f
-        colorResList.forEach { colorRes ->
-            turnTableSectorPaints.add(Paint().apply {
-                isAntiAlias = true
-                style = Paint.Style.FILL
-                color = obtainColorRes(colorRes)
-            })
-        }
-        sectorSweepAngle = 360f / turnTableSectorPaints.size
+        sweepAngle = 360f / turnTableContentList.size
     }
-
-    // 画字的笔
-    private var testPaint: Paint = Paint().apply {
-        color = Color.BLACK
-        isAntiAlias = true
-        textSize = 36f
-        textAlign = Paint.Align.CENTER
-    }
-
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -134,7 +162,6 @@ class TurnTableView @JvmOverloads constructor(
             MeasureSpec.AT_MOST -> min(defaultSize, measureSize)
             // 无约束模式：直接用默认尺寸
             MeasureSpec.UNSPECIFIED -> defaultSize
-            // 默认情况
             else -> defaultSize
         }
     }
@@ -144,94 +171,60 @@ class TurnTableView @JvmOverloads constructor(
         Log.e(TAG, "onSizeChanged  width:$w,height:${h}")
         viewWidth = w.toFloat()
         viewHeight = h.toFloat()
-        centerCircleX = (w / 2).toFloat()
-        centerCircleY = (h / 2).toFloat()
+        circleX = (w / 2).toFloat()
+        circleY = (h / 2).toFloat()
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         // 画背景
-        canvas.drawCircle(centerCircleX, centerCircleY, circleRadius, turnTableBackGroundPaint)
+        canvas.drawCircle(circleX, circleY, circleRadius, turnTableBackGroundPaint)
         // 画边框
-        canvas.drawCircle(centerCircleX, centerCircleY, circleRadius, turnTableStrokePaint)
-
-        turnTableSectorPaints.forEachIndexed { index, paint ->
+        canvas.drawCircle(circleX, circleY, circleRadius, turnTableStrokePaint)
+        // 画扇形 和 扇形上的文本
+        turnTableContentList.forEachIndexed { index, text ->
+            turnAnglePaint.color = colorResList[index]
             canvas.drawArc(
-                centerCircleX - circleRadius,
-                centerCircleY - circleRadius,
-                centerCircleX + circleRadius,
-                centerCircleY + circleRadius,
-                sectorStartAngle, sectorSweepAngle, true, paint
+                circleX - circleRadius,
+                circleY - circleRadius,
+                circleX + circleRadius,
+                circleY + circleRadius,
+                startAngle, sweepAngle, true, turnAnglePaint
             )
-            drawText(
-                canvas,
-                sectorStartAngle,
-                sectorSweepAngle,
-                circleRadius.toInt(),
-                turnTableContentList[index],
-                testPaint,
-            )
-            sectorStartAngle += sectorSweepAngle
+            canvas.drawText(text)
+            startAngle += sweepAngle
         }
     }
 
-    //绘制文字
-    private fun drawText(
-        canvas: Canvas,
-        startAngle: Float,
-        sweepAngle: Float,
-        radius: Int,
-        string: String,
-        textPaint: Paint,
-    ) {
+    private fun Canvas.drawText(text: String) {
         // 计算文本位置（和之前一致）
         val textAngle = startAngle + sweepAngle / 2
-        val textRadius = radius * 0.7f
+        // 距离圆心的距离
+        val textRadius = circleRadius * 0.7f
         val radian = Math.toRadians(textAngle.toDouble())
-        val textX = centerCircleX + (textRadius * cos(radian)).toFloat()
-        val textY = centerCircleY + (textRadius * sin(radian)).toFloat()
+        val textX = circleX + (textRadius * cos(radian)).toFloat()
+        val textY = circleY + (textRadius * sin(radian)).toFloat()
         // 1. 保存当前画布状态
-        canvas.save()
+        save()
         // 2. 将画布原点平移到文本绘制的位置
-        canvas.translate(textX, textY)
+        translate(textX, textY)
         // 3. 旋转画布,让文本垂直于扇形径向 ,如果你想让文本和扇形径向一致，直接用 textAngle 即可
-        canvas.rotate(textAngle + 90f)
+        rotate(textAngle + 90f)
         // 4. 绘制文本（此时原点已经平移到textX/textY，所以坐标填0,0）
         val fontMetrics = textPaint.fontMetrics
         val baseLineY = 0f - (fontMetrics.ascent + fontMetrics.descent) / 2
-        canvas.drawText(string, 0f, baseLineY, textPaint)
+        drawText(text, 0f, baseLineY, textPaint)
         // 5. 恢复画布状态（必须！否则后续绘制会继承旋转状态）
-        canvas.restore()
+        restore()
     }
 
     fun setScrollToPos(pos: Int) {
-        Log.e(TAG, "pos:$pos,")
-        var endAngle = 270 - sectorSweepAngle * pos
-        Log.e(TAG, "sectorStartAngle:$sectorStartAngle,")
-        if (endAngle < sectorStartAngle) {
-            endAngle += 4 * 360
-        } else {
-            endAngle += 3 * 360
-        }
-        Log.e(TAG,"end:${endAngle},")
-        val animator = ValueAnimator.ofFloat(sectorStartAngle, endAngle).apply {
-            setDuration(2000)
-            interpolator = DecelerateInterpolator()
-            addUpdateListener {
-                val value: Float = it.animatedValue as Float
-                //控制sectorStartAngle在0到360之间
-                sectorStartAngle = (value % 360 + 360) % 360
-                Log.e(TAG, "animatedValue:${sectorStartAngle}")
-                invalidate()
-            }
-            addListener(onStart = {
-
-            }, onEnd = {
-
-            })
-        }
-        animator.start()
+        Log.e(TAG, "pos:$pos")
+        val endAngle = sweepAngle * pos + 5 * 360
+        Log.e(TAG, "startAngle:$startAngle,endAngle:$endAngle")
+        valueAnimator.setFloatValues(startAngle, endAngle)
+        valueAnimator.start()
     }
 
     private fun obtainColorRes(@ColorRes colorRes: Int) = ContextCompat.getColor(context, colorRes)
